3b0be88291e5e0f2aec7a30d26fdf9c85f4c45b2
JorgeCardona <jorgecardona@utp.edu.co>
2024-10-02 22:45:53 -0500

    added PACKAGE_VERSION variable

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-# python3 setup.py sdist bdist_wheel
+import os
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as file:
     long_description = file.read()
 
+# Get version from environment variable or fallback to a default value
+version = os.getenv("PACKAGE_VERSION", "0.0.7")  # Default to '0.0.7' if not provided
+
 setuptools.setup(
     name="git-history-analyzer",
-    version="0.0.7",
+    version=version,  # Use the dynamic version from the environment variable
     author="Jorge Cardona",
-    description="Descripción del paquete",
+    description="Generate Reports from Your Repository's Git History",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jorgecardona/git-history-analyzer",
